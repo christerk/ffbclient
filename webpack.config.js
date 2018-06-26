437581cce3f739da31391711cfc91aa5b93f9a25
mike <mike@mike-work-linux>
2018-06-26 17:59:39 +0100

    progress on client load

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry:'./app.ts',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        library: 'EntryPoint'
     },
     plugins: [
         new webpack.DefinePlugin({
@@ -34,45 +35,93 @@ module.exports = {
         port:8080,
         disableHostCheck:true,
         before: function(app) {
+            var https = require('https');
+            var fs = require('fs');
+            var querystring = require('querystring');
+            var host = "fumbbl.com";
+            var client_id = "";
+
+            app.get("/matches", function(req_in, res) {
+                    var matches_options = {
+                        host: host,
+                        path:"/api/match/current",
+                        method:"GET"
+                    };
+                
+                matches_callback = function(response) {
+                    var str = "";
+                    response.on('data', function (data) {
+                        str += data;
+                    });
+
+                    response.on('end', function () {
+                        res.json(JSON.parse(str));
+                    });
+                };
+
+                var matches_req = https.request(matches_options, matches_callback);
+                matches_req.end();
+
+            });
             app.get("/auth", function(req_in, res) {
-                var fs = require('fs');
-                var https = require('https');
-                var querystring = require('querystring');
                 fs.readFile('auth.json', function(err, data) {
                     if (err) throw err;
                     var json_data = JSON.parse(data);
+                    client_id = json_data.client_id;
 
                     var form = {
                         grant_type: "client_credentials",
-                        client_id: json_data.client_id, 
+                        client_id: client_id,
                         client_secret: json_data.secret, 
                     };
                     var formData = querystring.stringify(form);
                     var contentLength = formData.length;
 
-                    var options = {
-                        host:"fumbbl.com",
+                    var bearer_options = {
+                        host: host,
                         path:"/api/oauth/token",
                         method:"POST",
                         headers:{"Content-Type":"application/x-www-form-urlencoded" }
                     };
 
-                    callback = function(response) {
+                    auth_callback = function(response) {
                         var str = "";
-                        var bearer;
                         response.on('data', function (data) {
                             str += data;
                         });
 
                         response.on('end', function () {
-                            bearer = JSON.parse(str);
-                            res.json(bearer);
+                            res.json({client_id: client_id, token:JSON.parse(str)});
+                        });
+                    };
+
+                    bearer_callback = function(response) {
+                        var str = "";
+                        response.on('data', function (data) {
+                            str += data;
+                        });
+
+                        response.on('end', function () {
+                            var bearer = JSON.parse(str);
+                            var auth_token_options = {
+                                host: host,
+                                path:"/api/auth/getToken",
+                                method:"POST",
+                                headers:{
+                                    accept: "application/json",
+                                    Authorization: " Bearer " + bearer.access_token
+                                }
+
+                            };
+
+                            var auth_req = https.request(auth_token_options, auth_callback);
+                            auth_req.end();
                         });
                     }
 
-                    var req = https.request(options, callback);
-                    req.write(querystring.stringify(form));
-                    req.end();
+                    var bearer_req = https.request(bearer_options, bearer_callback);
+                    bearer_req.write(querystring.stringify(form));
+                    bearer_req.end();
                 });
             });
         }
