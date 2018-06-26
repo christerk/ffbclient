8e867d78359c30e15169dd34b9fc07ec401fe3f6
mike <mike@mike-work-linux>
2018-06-26 09:29:14 +0100

    setting up route correctly and returning json from auth get request

--- webpack.config.js
+++ webpack.config.js
@@ -33,52 +33,48 @@ module.exports = {
     devServer: {
         port:8080,
         disableHostCheck:true,
-        proxy: {
-            "/auth": {
-                bypass: function(req, res, proxyOptions) {
-                    var fs = require('fs');
-                    var https = require('https');
-                    var querystring = require('querystring');
-                    fs.readFile('auth.json', function(err, data) {
-                        if (err) throw err;
-                        var json_data = JSON.parse(data);
+        before: function(app) {
+            app.get("/auth", function(req_in, res) {
+                var fs = require('fs');
+                var https = require('https');
+                var querystring = require('querystring');
+                fs.readFile('auth.json', function(err, data) {
+                    if (err) throw err;
+                    var json_data = JSON.parse(data);
 
-                        var form = {
-                            grant_type: "client_credentials",
-                            client_id: json_data.client_id, 
-                            client_secret: json_data.secret, 
-                        };
-                        var formData = querystring.stringify(form);
-                        var contentLength = formData.length;
+                    var form = {
+                        grant_type: "client_credentials",
+                        client_id: json_data.client_id, 
+                        client_secret: json_data.secret, 
+                    };
+                    var formData = querystring.stringify(form);
+                    var contentLength = formData.length;
 
-                        var options = {
-                            host:"fumbbl.com",
-                            path:"/api/oauth/token",
-                            method:"POST",
-                            headers:{"Content-Type":"application/x-www-form-urlencoded" }
-                        };
+                    var options = {
+                        host:"fumbbl.com",
+                        path:"/api/oauth/token",
+                        method:"POST",
+                        headers:{"Content-Type":"application/x-www-form-urlencoded" }
+                    };
 
-                        callback = function(response) {
-                            var str = ''
-                            response.on('data', function (chunk) {
-                                str += chunk;
-                            });
+                    callback = function(response) {
+                        var str = "";
+                        var bearer;
+                        response.on('data', function (data) {
+                            str += data;
+                        });
 
-                            response.on('end', function () {
-                                console.log(str);
-                            });
-                        }
+                        response.on('end', function () {
+                            bearer = JSON.parse(str);
+                            res.json(bearer);
+                        });
+                    }
 
-                        var req = https.request(options, callback);
-                        req.write(querystring.stringify(form));
-                        req.end();
-
-
-                        console.log(json_data); 
-                    });
-
-                }
-            }
+                    var req = https.request(options, callback);
+                    req.write(querystring.stringify(form));
+                    req.end();
+                });
+            });
+        }
         }
     }
-}
