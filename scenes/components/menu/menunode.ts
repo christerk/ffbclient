6d52f1737577c1dda13ae7515049dc6916f6b79c
Christian Huber <huberc@butterbrot.org>
2018-09-17 20:04:45 +0200

    Intermediate commit

---
+++ scenes/components/menu/menunode.ts
@@ -0,0 +1,20 @@
+import * as Comp from '..';
+
+export class MenuNode extends Comp.UIComponent {
+
+    private delegate: Comp.LinearPanel;
+
+    create(): Phaser.GameObjects.GameObject {
+        return undefined;
+    }
+
+    destroy(): void {
+    }
+
+    hide(): void {
+    }
+
+    show(): void {
+    }
+
+}
---
+++ scenes/components/menu/menupanel.ts
@@ -0,0 +1,20 @@
+import * as Comp from '..';
+
+export class MenuPanel extends Comp.UIComponent {
+
+    private delegate: Comp.LinearPanel;
+
+    create(): Phaser.GameObjects.GameObject {
+        return undefined;
+    }
+
+    destroy(): void {
+    }
+
+    hide(): void {
+    }
+
+    show(): void {
+    }
+
+}
