591304694be2ae2df37c855806082a687e358295
Christian Huber <christian.huber@sueddeutsche.de>
2019-04-13 23:31:12 +0200

    Added jest config.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "/tests/.*\.test\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ]
+}
